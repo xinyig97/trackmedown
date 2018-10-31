042a9ef73d447148dbc3cf0b3d67d76a3bc79aea
Xinyiguo2 <xinyig2@illinois.edu>
2018-10-31 12:26:21 -0500

    clean up some

--- try1.py
+++
@@ -1,9 +0,0 @@
-inp = input('giveme your file:')
-fhand = open(inp)
-for line in fhand:
-    if 'Invalid' in line:
-        line.rstrip()
-        pos = line.find('from ')
-        user = line[pos+5:]
-        print(user)
-        
